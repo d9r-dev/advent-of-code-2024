125a472d21f8cbd58d7d1f05891e43e18116edba
Danny Krämer <d.kraemer@dotsource.de>
2024-12-02 23:48:27 +0100

    day 2 part 2

--- src/aoc2024/day2.rs
+++ src/aoc2024/day2.rs
@@ -3,6 +3,7 @@ use std::fs;
 pub fn run_day2() {
     let contents = fs::read_to_string("inputs/day2/input1").expect("Could not read file");
     run_part_1(&contents);
+    run_part_2(&contents);
 }
 
 pub fn run_part_1(input: &str) {
@@ -43,4 +44,67 @@ pub fn run_part_1(input: &str) {
         }
     });
     println!("Day 2 Part 1: {}", result);
+}
+
+pub fn run_part_2(input: &str) {
+    let list = input.lines().collect::<Vec<&str>>();
+    let mut result: i32 = 0;
+    list.iter().for_each(|line| {
+        let levels = line
+            .split_whitespace()
+            .map(|char| char.parse::<i32>().unwrap())
+            .collect();
+        let valid = check_validity(&levels);
+        result = match valid {
+            true => result + 1,
+            false => {
+                let mut temp = 0;
+                for id_to_drop in 0..levels.len() {
+                    let mut new_levels = levels.clone();
+                    new_levels.remove(id_to_drop);
+                    let valid = check_validity(&new_levels);
+                    match valid {
+                        true => {
+                            temp = 1;
+                            break;
+                        }
+                        false => temp = 0,
+                    }
+                }
+                result + temp
+            }
+        }
+    });
+    println!("Day 2 Part 1: {}", result);
+}
+
+fn check_validity(line: &Vec<i32>) -> bool {
+    let mut forward: bool = true;
+    let mut valid: bool = true;
+    let mut before: i32 = 0;
+    for (i, num) in line.into_iter().enumerate() {
+        if i == 1 {
+            if *num == before {
+                valid = false;
+                break;
+            }
+            if *num < before {
+                forward = false;
+            }
+        }
+        if i > 0 {
+            if forward && *num > before && (num - before).abs() <= 3 {
+                before = *num;
+                continue;
+            } else if !forward && *num < before && (num - before).abs() <= 3 {
+                before = *num;
+                continue;
+            } else {
+                valid = false;
+                break;
+            }
+        }
+        before = *num;
+    }
+    valid
 }
