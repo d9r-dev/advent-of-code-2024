4b99ce85375aac0eaa02b701d284ae42ef9129d7
Danny Krämer <danny-kraemer@outlook.de>
2024-12-11 20:39:24 +0100

    day 6

--- src/aoc2024/day6.rs
+++ src/aoc2024/day6.rs
@@ -1,89 +1,156 @@
-use std::fs;
-
-type Grid = Vec<Vec<char>>;
-
-enum Direction {
-    Top,
-    Bottom,
-    Left,
-    Right,
+#[derive(Debug, Clone)]
+struct State {
+    pos: (i32, i32),
+    dir: (i32, i32),
 }
 
-struct Puzzle {
-    positions: Grid,
+#[derive(Debug, Clone)]
+struct Game {
+    moves: i32,
+    visited: Vec<State>,
+    guard: Guard,
+    obstacles: Vec<(i32, i32)>,
+    legal_moves: Vec<(i32, i32)>,
 }
 
-impl Puzzle {
-    fn new(contents: String) -> Puzzle {
-        let lines = contents.split("\n").collect::<Vec<&str>>();
-        let mut positions = vec![];
-        lines
+#[derive(Debug, Clone)]
+struct Guard {
+    pos: (i32, i32),
+    dir: (i32, i32),
+    start: (i32, i32),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+enum GameState {
+    Running,
+    GuardExited,
+    GuardLooped,
+}
+
+impl Game {
+    fn move_guard(&mut self) -> GameState {
+        if self
+            .visited
             .iter()
-            .for_each(|&l| positions.push(Vec::from(l.chars().collect::<Vec<char>>())));
-        Puzzle { positions }
-    }
-
-    fn run_part1(&self) -> usize {
-        let mut current_pos = self.find_start().unwrap();
-        let mut current_dir = Direction::Top;
-        let mut visited_pos = vec![];
-        loop {
-            let next_position = Puzzle::get_next_position(current_pos, &current_dir);
-
-            match self.get_char(next_position.0, next_position.1) {
-                Some('#') => {
-                    current_dir = match current_dir {
-                        Direction::Top => Direction::Right,
-                        Direction::Bottom => Direction::Left,
-                        Direction::Left => Direction::Top,
-                        Direction::Right => Direction::Bottom,
-                    };
-                    if !visited_pos.contains(&current_pos) {
-                        visited_pos.push(current_pos);
-                    }
-                    current_pos = Self::get_next_position(current_pos, &current_dir);
-                }
-                Some('.') | Some('^') => {
-                    if !visited_pos.contains(&current_pos) {
-                        visited_pos.push(current_pos);
-                    }
-                    current_pos = next_position;
-                }
-                Some('\n') | None => break,
-                _ => break,
-            }
+            .any(|x| x.pos == self.guard.pos && x.dir == self.guard.dir)
+        {
+            return GameState::GuardLooped;
         }
 
-        visited_pos.len() + 1
-    }
+        let state = State {
+            pos: self.guard.pos,
+            dir: self.guard.dir,
+        };
+        self.visited.push(state);
 
-    fn get_next_position(current_pos: (isize, isize), current_dir: &Direction) -> (isize, isize) {
-        match current_dir {
-            Direction::Top => (current_pos.0, current_pos.1 - 1),
-            Direction::Bottom => (current_pos.0, current_pos.1 + 1),
-            Direction::Left => (current_pos.0 - 1, current_pos.1),
-            Direction::Right => (current_pos.0 + 1, current_pos.1),
+        let mut next_pos_in_same_dir = (
+            self.guard.pos.0 + self.guard.dir.0,
+            self.guard.pos.1 + self.guard.dir.1,
+        );
+
+        while self.obstacles.contains(&next_pos_in_same_dir) {
+            // rotate 90 degrees to the right
+            self.guard.dir = (-self.guard.dir.1, self.guard.dir.0);
+            next_pos_in_same_dir = (
+                self.guard.pos.0 + self.guard.dir.0,
+                self.guard.pos.1 + self.guard.dir.1,
+            );
         }
-    }
-    fn get_char(&self, x: isize, y: isize) -> Option<char> {
-        self.positions.get(y as usize)?.get(x as usize).copied()
-    }
-    fn find_start(&self) -> Option<(isize, isize)> {
-        for (y, line) in self.positions.iter().enumerate() {
-            for (x, char) in line.iter().enumerate() {
-                if *char == '^' {
-                    return Some((x as isize, y as isize));
-                }
-            }
+
+        self.guard.pos = next_pos_in_same_dir;
+
+        if self.legal_moves.contains(&next_pos_in_same_dir) {
+            self.moves += 1;
+            return GameState::Running;
         }
-        None
+
+        GameState::GuardExited
     }
 }
-
 pub fn run_day6() {
-    let contents = fs::read_to_string("inputs/day6/input").expect("Could not read file");
-    let puzzle = Puzzle::new(contents);
-    let result1 = puzzle.run_part1();
+    let input = include_str!("../../inputs/day6/input");
 
-    println!("Part 1: {}", result1);
+    let mut pos = (0, 0);
+    let game = input.chars().fold(
+        Game {
+            moves: 0,
+            visited: vec![],
+            legal_moves: vec![],
+            guard: Guard {
+                pos: (0, 0),
+                dir: (0, 0),
+                start: (0, 0),
+            },
+            obstacles: vec![],
+        },
+        |mut acc, x| {
+            acc.legal_moves.push(pos);
+            match x {
+                '#' => {
+                    acc.obstacles.push(pos);
+                    pos = (pos.0 + 1, pos.1);
+                }
+                '^' => {
+                    acc.guard.start = pos;
+                    acc.guard.pos = pos;
+                    acc.guard.dir = (0, -1);
+                    pos = (pos.0 + 1, pos.1);
+                }
+                'v' => {
+                    acc.guard.start = pos;
+                    acc.guard.pos = pos;
+                    acc.guard.dir = (0, 1);
+                    pos = (pos.0 + 1, pos.1);
+                }
+                '<' => {
+                    acc.guard.start = pos;
+                    acc.guard.pos = pos;
+                    acc.guard.dir = (-1, 0);
+                    pos = (pos.0 + 1, pos.1);
+                }
+                '>' => {
+                    acc.guard.start = pos;
+                    acc.guard.pos = pos;
+                    acc.guard.dir = (1, 0);
+                    pos = (pos.0 + 1, pos.1);
+                }
+                '\n' => pos = (0, pos.1 + 1),
+                '.' => pos = (pos.0 + 1, pos.1),
+                _ => panic!("Invalid character"),
+            };
+            acc
+        },
+    );
+
+    let mut game1 = game.clone();
+    while game1.move_guard() == GameState::Running {}
+
+    let visited = game1.visited.iter().collect::<Vec<&State>>();
+    let mut counter = 0;
+
+    let mut x = visited
+        .iter()
+        .map(|x| {
+            counter = counter + 1;
+            println!("{:?}", counter);
+            let mut game2 = game.clone();
+            if x.pos == game2.guard.start {
+                return (GameState::GuardExited, x.pos);
+            }
+            game2.obstacles.push(x.pos);
+            let mut game_state = GameState::Running;
+            while game_state == GameState::Running {
+                game_state = game2.move_guard();
+            }
+            game2.obstacles.pop();
+            (game_state, x.pos)
+        })
+        .filter(|x| &x.0 == &GameState::GuardLooped)
+        .map(|x| x.1)
+        .collect::<Vec<(i32, i32)>>();
+
+    x.sort();
+    x.dedup();
+
+    println!("{:?}", x.len());
 }
